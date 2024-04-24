65e07e481b0825f1233eb796ab14bd1f3585b69f
Kyle <kyleyee522@gmail.com>
2024-04-23 19:37:03 -0700

    added encryption to passwords and tokens

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
+const express = require('express');
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const jwt = require('jsonwebtoken');
 
 router.use('/api', apiRoutes);
+
+// Test a protected route to check if the user is logged in
+router.get('/protected', (req, res) => {
+	console.log(req.headers);
+	// const token = req.headers?.authorization?.split(' ')[1];
+	const token = req.headers?.authorization?.split(' ')[0];
+	console.log('==============================');
+	console.log(token);
+
+	try {
+		const tokenDecoded = jwt.verify(token, process.env.TOKEN_SECRET);
+		res.json({ msg: 'welcome to the club!', tokenDecoded });
+	} catch (error) {
+		res.status(403).json({ msg: 'invalid token' });
+	}
+});
 
 router.use((req, res) => {
 	return res.send('Wrong route!');
